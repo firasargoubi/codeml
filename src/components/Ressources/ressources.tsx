a5ce4d1e4e8822b472a28e3c4708c3222ddc73f2
Firas Argoubi <99605613+firasargoubi@users.noreply.github.com>
2025-09-06 15:11:38 -0400

    Add carousel for resources

--- src/components/Ressources/ressources.tsx
+++ src/components/Ressources/ressources.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
 import ContainerHeading from '../common/container-heading';
 import './ressources.scss';
 
@@ -13,21 +15,27 @@ const Ressources = () => {
   const ressourcesLinks: Link[] = t('Ressources.links', {
     returnObjects: true,
   });
+  const settings = {
+    arrows: false,
+    dots: false,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className="ressources">
       <ContainerHeading title="Ressources" />
       <div>
         <p className="ressources-text">{t('Ressources.text')}</p>
-        <ul>
+        <Slider {...settings} className="ressources-slider">
           {ressourcesLinks.map((item, index) => (
-            <li key={index}>
-              <p className="ressources-text">
-                {' '}
-                <a href={item.link}>{item.link}</a> ({item.helperText})
-              </p>
-            </li>
+            <div key={index}>
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                <div className="resource-card">{item.helperText}</div>
+              </a>
+            </div>
           ))}
-        </ul>
+        </Slider>
       </div>
     </div>
   );
